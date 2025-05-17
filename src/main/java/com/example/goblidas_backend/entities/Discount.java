cf7238c14fcef1b7de59b83826f0f75ef11be6d5
Naza69 <fiorettinazareno@gmail.com>
2025-05-17 00:06:44 -0300

    Retoque formateo de tipo fecha

--- src/main/java/com/example/goblidas_backend/entities/Discount.java
+++ src/main/java/com/example/goblidas_backend/entities/Discount.java
@@ -1,5 +1,6 @@
 package com.example.goblidas_backend.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -13,9 +14,12 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class Discount extends Base {
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH-mm-ss")
     @Column(name = "fecha_final")
     private LocalDateTime finalDate;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH-mm-ss")
     @Column(name = "fecha_inicio")
     private LocalDateTime initialDate;
 
